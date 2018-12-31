060de4f66f8f5dc9923ff318a2ebf4807f363a64
Alessandro Verdura <alessandro@compucorp.co.uk>
2018-12-31 15:31:03 +0100

    hide some of the top filters when < 1024

--- src/components/top-form.js
+++ src/components/top-form.js
@@ -1,22 +1,33 @@
 import React from 'react'
+import Media from 'react-media';
 import { Button, Form, Icon, Switch } from 'antd'
 
 class TopForm extends React.Component {
   render () {
-    return (<Form layout="inline">
-      <Form.Item>
-        <span>Record</span>
-      </Form.Item>
-      <Form.Item>
-        <Button.Group>
-          <Button>Absence <Icon type="user" /></Button>
-          <Button>Overtime <Icon type="clock-circle" /></Button>
-        </Button.Group>
-      </Form.Item>
-      <Form.Item label="Show Filters">
-        <Switch checked={this.props.showFilters} onChange={this.props.cb} size="small" />
-      </Form.Item>
-    </Form>)
+    return (
+      <Media query="(max-width: 1024px)">
+        {matches => (
+          <Form layout="inline">
+            {!matches &&
+              <Form.Item>
+                <span>Record</span>
+              </Form.Item>
+            }
+            {!matches &&
+              <Form.Item>
+                <Button.Group>
+                  <Button>Absence <Icon type="user" /></Button>
+                  <Button>Overtime <Icon type="clock-circle" /></Button>
+                </Button.Group>
+              </Form.Item>
+            }
+            <Form.Item label="Show Filters">
+              <Switch checked={this.props.showFilters} onChange={this.props.cb} size="small" />
+            </Form.Item>
+          </Form>
+        )}
+      </Media>
+    )
   }
 }
 
