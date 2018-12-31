e7eb466ee93a1cf9b4e39b8ac80d7c81f81eb732
Alessandro Verdura <alessandro@compucorp.co.uk>
2018-12-31 15:26:10 +0100

    responsiveness tweaks

--- src/App.js
+++ src/App.js
@@ -118,99 +118,101 @@ export default class App extends React.Component {
   render() {
     return (
       <Layout>
-        <Layout.Sider
-          breakpoint="md"
-          width={240}
-          theme="light">
-          <div style={{ textAlign: 'center' }}>
-            <h1>
-              <Icon type="calendar" /><br />Leave
-            </h1>
-          </div>
-          <MainMenu />
-        </Layout.Sider>
-        <Layout.Content style={{ padding: '25px 36px' }}>
-          <Media query="(max-width: 1280px)">
-            {matches => (
-              <>
-                <Breadcrumb style={{ marginBottom: 35 }}>
-                   <Breadcrumb.Item>CiviCloud</Breadcrumb.Item>
-                  <Breadcrumb.Item>Leave</Breadcrumb.Item>
-                  <Breadcrumb.Item>Leave Requests</Breadcrumb.Item>
-                </Breadcrumb>
-                <div>
-                  <Row gutter={8} style={{ marginBottom: 29 }}>
-                    <Col span={12}>
-                      <h2 style={{ marginBottom: 0 }}><Icon type="calendar" />Leave Requests</h2>
-                    </Col>
-                    <Col span={12}>
-                      <div style={{ float: 'right' }}>
-                        <TopForm showFilters={this.state.showFilters} cb={this.toggleFiltersVisibility.bind(this)} />
-                      </div>
-                    </Col>
-                  </Row>
-                  <Row gutter={8}>
-                    <Col span={this.state.showFilters && !matches ? 18 : 24}>
-                      <Card bodyStyle={{ paddingBottom: 0 }}>
-                        <Row style={{ marginBottom: 20 }}>
-                          <Col span={12}>
-                            <Select defaultValue={0} dropdownMatchSelectWidth={false} style={{ width: 200 }}>
-                              <Select.Option value={0}>Actions</Select.Option>
-                              <Select.Option value={2}>Bulk Action #1</Select.Option>
-                              <Select.Option value={3}>Bulk Action #2</Select.Option>
-                              <Select.Option value={4}>Bulk Action #3</Select.Option>
+        <Media query="(max-width: 1024px)">
+          {matches => (
+            <Layout.Sider
+              breakpoint="md"
+              width={matches ? 216 : 240}
+              theme="light">
+              <div style={{ textAlign: 'center' }}>
+                <h1>
+                  <Icon type="calendar" /><br />Leave
+                </h1>
+              </div>
+              <MainMenu />
+            </Layout.Sider>
+          )}
+        </Media>
+        <Media query="(max-width: 1280px)">
+          {matches => (
+            <Layout.Content style={{ padding: matches ? '25px 16px' : '25px 36px' }}>
+              <Breadcrumb style={{ marginBottom: 35 }}>
+                <Breadcrumb.Item>CiviCloud</Breadcrumb.Item>
+                <Breadcrumb.Item>Leave</Breadcrumb.Item>
+                <Breadcrumb.Item>Leave Requests</Breadcrumb.Item>
+              </Breadcrumb>
+              <div>
+                <Row gutter={8} style={{ marginBottom: 29 }}>
+                  <Col span={8}>
+                    <h2 style={{ marginBottom: 0 }}><Icon type="calendar" />Leave Requests</h2>
+                  </Col>
+                  <Col span={16}>
+                    <div style={{ float: 'right' }}>
+                      <TopForm showFilters={this.state.showFilters} cb={this.toggleFiltersVisibility.bind(this)} />
+                    </div>
+                  </Col>
+                </Row>
+                <Row gutter={8}>
+                  <Col span={this.state.showFilters && !matches ? 18 : 24}>
+                    <Card bodyStyle={{ paddingBottom: 0 }}>
+                      <Row style={{ marginBottom: 20 }}>
+                        <Col span={12}>
+                          <Select defaultValue={0} dropdownMatchSelectWidth={false} style={{ width: 200 }}>
+                            <Select.Option value={0}>Actions</Select.Option>
+                            <Select.Option value={2}>Bulk Action #1</Select.Option>
+                            <Select.Option value={3}>Bulk Action #2</Select.Option>
+                            <Select.Option value={4}>Bulk Action #3</Select.Option>
+                          </Select>
+                        </Col>
+                        <Col span={12}>
+                          <div style={{ float: 'right' }}>
+                            <Select
+                              defaultValue="contacts">
+                              <Select.Option value="contacts">Contacts</Select.Option>
+                              <Select.Option value="organizations">Organizations</Select.Option>
                             </Select>
-                          </Col>
-                          <Col span={12}>
-                            <div style={{ float: 'right' }}>
-                              <Select
-                                defaultValue="contacts">
-                                <Select.Option value="contacts">Contacts</Select.Option>
-                                <Select.Option value="organizations">Organizations</Select.Option>
-                              </Select>
-                              <Select
-                                style={{ width: 230 }}
-                                placeholder="Search / Filter"
-                                showArrow={false}>
-                                {dataSource.map(user => (
-                                  <Select.Option key={user.key} value={user.id}>{user.name}</Select.Option>
-                                ))}
-                              </Select>
-                            </div>
-                          </Col>
-                        </Row>
-                        <Table
-                          style={{ marginLeft: -24, marginRight: -24 }}
-                          rowSelection={rowSelection}
-                          dataSource={dataSource}
-                          columns={columns}
-                          pagination={false}
-                          scroll={{ y: 800 }}
-                          size="small" />
-                      </Card>
-                    </Col>
-                    <Col span={this.state.showFilters && !matches ? 6 : 0}>
-                      <Card style={{ background: '#e6e6e6' }}>
-                        <FiltersAside />
-                      </Card>
-                    </Col>
-                  </Row>
-                </div>
-                { matches &&
-                  <Drawer
-                  title="Filters"
-                  placement="right"
-                  closable={true}
-                  visible={this.state.showFilters}
-                  onClose={this.toggleFiltersVisibility.bind(this, false)}
-                  width="300">
-                    <FiltersAside />
-                  </Drawer>
-                }
-              </>
-            )}
-          </Media>
-        </Layout.Content>
+                            <Select
+                              style={{ width: 230 }}
+                              placeholder="Search / Filter"
+                              showArrow={false}>
+                              {dataSource.map(user => (
+                                <Select.Option key={user.key} value={user.id}>{user.name}</Select.Option>
+                              ))}
+                            </Select>
+                          </div>
+                        </Col>
+                      </Row>
+                      <Table
+                        style={{ marginLeft: -24, marginRight: -24 }}
+                        rowSelection={rowSelection}
+                        dataSource={dataSource}
+                        columns={columns}
+                        pagination={false}
+                        scroll={{ y: 800 }}
+                        size="small" />
+                    </Card>
+                  </Col>
+                  <Col span={this.state.showFilters && !matches ? 6 : 0}>
+                    <Card style={{ background: '#e6e6e6' }}>
+                      <FiltersAside />
+                    </Card>
+                  </Col>
+                </Row>
+              </div>
+              { matches &&
+                <Drawer
+                title="Filters"
+                placement="right"
+                closable={true}
+                visible={this.state.showFilters}
+                onClose={this.toggleFiltersVisibility.bind(this, false)}
+                width="300">
+                  <FiltersAside />
+                </Drawer>
+              }
+            </Layout.Content>
+          )}
+        </Media>
       </Layout>
     );
   }
