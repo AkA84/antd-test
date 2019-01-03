0941bf6ee366c6a10c2f64d6a56762150e452e10
Alessandro Verdura <alessandro@compucorp.co.uk>
2019-01-03 13:50:42 +0100

    Updates

--- precache-manifest.1d60ff31109c9fa70123916878dafbc7.js
+++ precache-manifest.1d60ff31109c9fa70123916878dafbc7.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/antd-test/static/js/runtime~main.4bb76e5a.js"
   },
   {
-    "revision": "8677135f13d1064535c7",
-    "url": "/antd-test/static/js/main.8677135f.chunk.js"
+    "revision": "8a2c2ea143e773fff714",
+    "url": "/antd-test/static/js/main.8a2c2ea1.chunk.js"
   },
   {
     "revision": "fff9d6e10b38de7374e1",
     "url": "/antd-test/static/js/1.fff9d6e1.chunk.js"
   },
   {
-    "revision": "8677135f13d1064535c7",
-    "url": "/antd-test/static/css/main.bbfe1de2.chunk.css"
+    "revision": "8a2c2ea143e773fff714",
+    "url": "/antd-test/static/css/main.b68b023f.chunk.css"
   },
   {
     "revision": "fff9d6e10b38de7374e1",
     "url": "/antd-test/static/css/1.38b0ecbb.chunk.css"
   },
   {
-    "revision": "daf9c88ef404d513d8fb1437636b88ad",
+    "revision": "3faeb9f78046f7a2603b4e449aec2669",
     "url": "/antd-test/index.html"
   }
 ];
